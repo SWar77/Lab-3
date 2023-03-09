7378d28375d1e2175af3ba267c0b78032d17b1ab
SWar77 <124618670+SWar77@users.noreply.github.com>
2023-03-08 18:05:15 -0600

    Add files via upload

---
+++ Tare-parcial-progra.ino
@@ -0,0 +1,337 @@
+int a= 5; // Pines conectados al Display de 7 segmentos ánodo común
+int b= 6;
+int c= 7;
+int d= 8;
+int e= 9;
+int f= 10;
+int g= 11;
+
+int in1; // Conteo ascendente
+int in2; // Conteo descendente
+
+char x; // Variable para comunicación serial
+
+int t = 500;
+void off(){         // Apagar display
+ digitalWrite(a, 0); 
+ digitalWrite(b, 0);
+ digitalWrite(c, 0);
+ digitalWrite(d, 0);
+ digitalWrite(e, 0);
+ digitalWrite(f, 0);
+ digitalWrite(g, 0);
+}
+
+void setup(){
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+
+  pinMode(4, INPUT);
+  pinMode(3, INPUT);
+
+  Serial.begin(9600);
+  off();
+}
+
+void loop(){
+  
+  in1 = digitalRead(4);
+  in2 = digitalRead(3);
+
+if (Serial.available() > 0) {
+  x=Serial.read();
+   switch (x){
+    case '0':
+    digitalWrite(a, 1); // bloque del numero 0
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 1);
+    digitalWrite(f, 1);
+    digitalWrite(g, 0);
+    break;
+    case '1':
+    digitalWrite(a, 0); //bloque del numero 1
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 0);
+    digitalWrite(e, 0);
+    digitalWrite(f, 0);
+    digitalWrite(g, 0);
+    break;
+    case '2':
+    digitalWrite(a, 1); //bloque del numero 2
+    digitalWrite(b, 1);
+    digitalWrite(c, 0);
+    digitalWrite(d, 1);
+    digitalWrite(e, 1);
+    digitalWrite(f, 0);
+    digitalWrite(g, 1);
+    break;
+    case '3':
+    digitalWrite(a, 1); //bloque del numero 3
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 0);
+    digitalWrite(f, 0);
+    digitalWrite(g, 1);
+    break;
+    case '4':
+    digitalWrite(a, 0); //bloque del numero 4
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 0);
+    digitalWrite(e, 0);
+    digitalWrite(f, 1);
+    digitalWrite(g, 1);
+    break;
+    case '5':
+    digitalWrite(a, 1); //bloque del numero 5
+    digitalWrite(b, 0);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 0);
+    digitalWrite(f, 1);
+    digitalWrite(g, 1);
+    break;
+    case '6':
+    digitalWrite(a, 1); //bloque del numero 6
+    digitalWrite(b, 0);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 1);
+    digitalWrite(f, 1);
+    digitalWrite(g, 1);
+    break;
+    case '7':
+    digitalWrite(a, 1); //bloque del numero 7
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 0);
+    digitalWrite(e, 0);
+    digitalWrite(f, 0);
+    digitalWrite(g, 0);
+    break;
+    case '8':
+    digitalWrite(a, 1); //bloque del numero 8
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 1);
+    digitalWrite(f, 1);
+    digitalWrite(g, 1);
+    break;
+    case '9':
+    digitalWrite(a, 1); //bloque del numero 9
+    digitalWrite(b, 1);
+    digitalWrite(c, 1);
+    digitalWrite(d, 1);
+    digitalWrite(e, 0);
+    digitalWrite(f, 1);
+    digitalWrite(g, 1);
+    break; 
+    
+    case 'a':
+    digitalWrite(a, 0); // Apagar display
+    digitalWrite(b, 0);
+    digitalWrite(c, 0);
+    digitalWrite(d, 0);
+    digitalWrite(e, 0);
+    digitalWrite(f, 0);
+    digitalWrite(g, 0); 
+    break; 
+ }
+}if (in1 == LOW){
+  digitalWrite(a, 1); // bloque del numero 0
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 0);
+   delay(t);
+  
+  digitalWrite(a, 0); //bloque del numero 1
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 2
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 0);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 3
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 0); //bloque del numero 4
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 5
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 6
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 7
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 8
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+  digitalWrite(a, 1); //bloque del numero 9
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  off();
+ }else if (in2 == HIGH){
+   digitalWrite(a, 1); //bloque del numero 9
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+
+   digitalWrite(a, 1); //bloque del numero 8
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+    
+digitalWrite(a, 1); //bloque del numero 7
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+   delay(t);
+  
+    
+   digitalWrite(a, 1); //bloque del numero 6
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+  
+   
+   digitalWrite(a, 1); //bloque del numero 5
+  digitalWrite(b, 0);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+
+   digitalWrite(a, 0); //bloque del numero 4
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 1);
+  digitalWrite(g, 1);
+   delay(t);
+   
+   digitalWrite(a, 1); //bloque del numero 3
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 1);
+   delay(t);
+  
+   digitalWrite(a, 1); //bloque del numero 2
+  digitalWrite(b, 1);
+  digitalWrite(c, 0);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 0);
+  digitalWrite(g, 1);
+   delay(t);
+
+    digitalWrite(a, 0); //bloque del numero 1
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 0);
+  digitalWrite(e, 0);
+  digitalWrite(f, 0);
+  digitalWrite(g, 0);
+   delay(t);
+  
+
+   digitalWrite(a, 1); // bloque del numero 0
+  digitalWrite(b, 1);
+  digitalWrite(c, 1);
+  digitalWrite(d, 1);
+  digitalWrite(e, 1);
+  digitalWrite(f, 1);
+  digitalWrite(g, 0);
+   delay(t);
+   off();
+  }
+}
